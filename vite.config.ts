77048f6dec096dd756f158bb0ef06ba515726b16
norxxx <onori.black1984@gmail.com>
2025-10-06 21:13:39 +0900

    format: biome lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import path from "node:path";
 import tailwind from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
